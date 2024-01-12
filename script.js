91edc24ed2ed04c9f653c2bd9bb445ae36de7951
Sofía J. D. <59852487+SofiaJD@users.noreply.github.com>
2024-01-12 18:07:12 -0400

    Add game and result function

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let playerPoints = 0;
+let computerPoints = 0;
+
 function getComputerChoice()
 {
     const choices = ["rock", "paper", "scissor"];
@@ -6,23 +9,40 @@ function getComputerChoice()
     return randomChoice;
 }
 
+function result()
+{
+    if(playerPoints > computerPoints)
+    {
+        console.log("You won! 🏆");
+    }
+    else if(computerPoints > playerPoints)
+    {
+        console.log("You lose! 😥");
+    }
+    else
+    {
+        console.log("It's a tie! 🤝🏽");
+    }
+}
+
 function round(playerSelection, computerSelection)
 {
     playerSelection = playerSelection.toLowerCase();
-    // computerSelection = getComputerChoice();
 
     if(playerSelection === computerSelection)
     {
-        return "It's a Tie!"
+        return "It's a Tie!";
     }
     else if(computerSelection == "rock")
     {
         if(playerSelection == "scissor")
         {
+            computerPoints ++;
             return "You lose! Rock beats Scissor";
         }
         else if(playerSelection == "paper")
         {
+            playerPoints ++; 
             return "You won! Paper beats Rock";
         }
     }
@@ -30,10 +50,12 @@ function round(playerSelection, computerSelection)
     {
         if(playerSelection == "rock")
         {
+            computerPoints ++;
             return "You lose! Paper beats rock";
         }
         else if(playerSelection == "scissor")
         {
+            playerPoints ++; 
             return "You won! Scissor beats paper";
         }
     }
@@ -41,19 +63,32 @@ function round(playerSelection, computerSelection)
     {
         if(playerSelection == "paper")
         {
+            computerPoints ++;
             return "You lose! Scissor beats paper";
         }
         else if(playerSelection == "rock")
         {
+            playerPoints ++; 
             return "You won! Rock beats Scissor";
         }
     }
 }
 
-const playerSelection = "rock";
-const computerSelection = getComputerChoice();
+function game()
+{
+    for(let i = 0; i <= 5; i++)
+    {
+        playerSelection = prompt('Insert "Rock", "Paper" or "Scissor".').toLowerCase();
+        const computerSelection = getComputerChoice();
 
-console.log("Player choice: " + playerSelection);
-console.log("Computer choice: " + computerSelection);
+        console.log("Player choice: " + playerSelection);
+        console.log("Computer choice: " + computerSelection);
 
-console.log(round(playerSelection, computerSelection));
+        console.log(round(playerSelection, computerSelection));
+
+        console.log("Player's points: " + playerPoints + "\nComputer's points: " + computerPoints);
+    }
+    result();
+}
+
+game();
